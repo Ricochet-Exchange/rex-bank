924f023bc383e92edde85d79a3b67507e1d461cb
Brenda <l.loya.b@gmail.com>
2020-06-16 16:40:21 -0400

    comments

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.5.0;
 import "./BankStorage.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
-import "usingtellor/contracts/UsingTellor.sol";
+//import "usingtellor/contracts/UsingTellor.sol";
 
-
+/**
+* @title Bank
+* This contract allows the Bank owner to deposit reserves(debt token), earn interest and
+* origination fees and users can come collateralize and borrow against their collateral.
+* The oracle for Bank is Tellor.
+*/
 contract Bank is BankStorage, Ownable, UsingTellor {
-
+  /*Events*/
   event ReserveDeposit(uint256 amount);
   event ReserveWithdraw(address token, uint256 amount);
   event VaultDeposit(address owner, uint256 amount);
@@ -15,6 +20,7 @@ contract Bank is BankStorage, Ownable, UsingTellor {
   event VaultRepay(address borrower, uint256 amount);
   event VaultWithdraw(address borrower);
 
+  /*Constructor*/
   constructor(
     uint256 interestRate,
     uint256 originationFee,
@@ -44,12 +50,21 @@ contract Bank is BankStorage, Ownable, UsingTellor {
     _collateralTokenTellorRequestId = collateralTokenTellorRequestId;
   }
 
+  /*Functions*/
+  /**
+  * @dev This function allows the Bank owner to deposit the reserve (debt tokens)
+  * @param amount is the amount to deposit
+  */
   function reserveDeposit(uint256 amount) external onlyOwner {
     require(IERC20(_debtToken).transferFrom(msg.sender, address(this), amount));
     _debtReserveBalance += amount;
     emit ReserveDeposit(amount);
   }
 
+  /**
+  * @dev This function allows the Bank owner to withdraw the reserve (debt tokens)
+  * @param amount is the amount to withdraw
+  */
   //for reserveWithdraw or reserveWithdrawCollateral, should it not update the price and make sure it's collateralized?
   function reserveWithdraw(uint256 amount) external onlyOwner {
     require(_debtReserveBalance >= amount, "NOT ENOUGH DEBT TOKENS IN RESERVE");
@@ -58,6 +73,11 @@ contract Bank is BankStorage, Ownable, UsingTellor {
     emit ReserveWithdraw(_debtToken, amount);
   }
 
+  /**
+  * @dev This function allows the user to withdraw their collateral 
+  * @param amount is the amount to withdraw
+  */
+  //should the _collateralReserveBalance be adjusted to remove the origination fee and interest???
   function reserveWithdrawCollateral(uint256 amount) external onlyOwner {
     require(_collateralReserveBalance >= amount, "NOT ENOUGH COLLATERAL IN RESERVE");
     require(IERC20(_collateralToken).transfer(msg.sender, amount));
@@ -65,6 +85,10 @@ contract Bank is BankStorage, Ownable, UsingTellor {
     emit ReserveWithdraw(_collateralToken, amount);
   }
 
+  /**
+  * @dev Use this function to get and update the price for the collateral and debt token
+  * using the Tellor Oracle.
+  */
   function updatePrice() external{
     bool ifRetrieve;
     uint256 _timestampRetrieved;
@@ -72,11 +96,15 @@ contract Bank is BankStorage, Ownable, UsingTellor {
     (ifRetrieve, _collateralTokenPrice, _timestampRetrieved) = getDataBefore(_collateralTokenTellorRequestId,now - 1 hours);
   }
 
-
-  //should you make it so anyone can liquidate? 
+  /**
+  * @dev The Bank owner can use this function to liquidate debt and take the collateral
+  * @param vaultOwner is the user the bank owner wants to liquidate
+  */
+  //should you make it so anyone can liquidate? I think this makes sense, since anyone could run the fx but
+  //only the vaultowner gets the $$
   function liquidate(address vaultOwner) external onlyOwner {
     // Require undercollateralization
-    require(_getVaultCollateralizationRatio(vaultOwner) < _collateralizationRatio * 100, "VAULT NOT UNDERCOLLATERALIZED");
+    require(getVaultCollateralizationRatio(vaultOwner) < _collateralizationRatio * 100, "VAULT NOT UNDERCOLLATERALIZED");
     // TODO: Confirm this is calculated correctly
     uint256 debtOwned = vaults[vaultOwner].debtAmount + (vaults[vaultOwner].debtAmount * 100 * _liquidationPenalty / 100 / 100);
     uint256 collateralToLiquidate = debtOwned * _debtTokenPrice / _collateralTokenPrice;
@@ -86,8 +114,12 @@ contract Bank is BankStorage, Ownable, UsingTellor {
   }
 
 
+  /**
+  * @dev Use this function to allow users to deposit collateral to the vault
+  * @param amount is the collateral amount
+  */
   function vaultDeposit(uint256 amount) external {
-    //why does it have to be zero? can you not add?
+    //why does it have to be zero? can you not add???
     require(vaults[msg.sender].collateralAmount == 0, "ALREADY DEPOSITED COLLATERAL");
     require(IERC20(_collateralToken).transferFrom(msg.sender, address(this), amount));
     vaults[msg.sender].collateralAmount += amount;
@@ -95,6 +127,10 @@ contract Bank is BankStorage, Ownable, UsingTellor {
 
   }
 
+  /**
+  * @dev Use this function to allow users to borrow against their collateral
+  * @param amount to borrow
+  */
   function vaultBorrow(uint256 amount) external {
     require(vaults[msg.sender].debtAmount == 0, "ALREADY BORROWING");//can you not borrow more?
     require(vaults[msg.sender].collateralAmount != 0, "NO COLLATERAL");//isn't this require reduntant considering the maxBorrow/_debtReserveBalance statements?
@@ -104,15 +140,21 @@ contract Bank is BankStorage, Ownable, UsingTellor {
     require(amount < maxBorrow, "NOT ENOUGH COLLATERAL");
     require(amount <= _debtReserveBalance, "NOT ENOUGH RESERVES");
     vaults[msg.sender].debtAmount += amount + ((amount * _originationFee) / 100);
+    vaults[msg.sender].createdAt = block.timestamp;//probably no re-entrancy risk but general this should be moved bevore transrfer
     _debtReserveBalance -= amount;
     require(IERC20(_debtToken).transfer(msg.sender, amount));
-    vaults[msg.sender].createdAt = block.timestamp;
     emit VaultBorrow(msg.sender, amount);
   }
 
+  /**
+  * @dev This function allows users to pay the interest and origination fee to the
+  *  vault before being able to withdraw
+  * @param amount owed
+  */
   function vaultRepay(uint256 amount) external {
     vaults[msg.sender].debtAmount = getVaultRepayAmount();
-    require(amount <= vaults[msg.sender].debtAmount, "CANNOT REPAY MORE THAN OWED");
+    require(amount <= vaults[msg.sender].debtAmount, "CANNOT REPAY MORE THAN OWED");//I get that they should not pay more than they owe 
+    //but there is no harm if they do, then just allow them to  withdraw so long as debt< 0
     require(IERC20(_debtToken).transferFrom(msg.sender, address(this), amount));
     vaults[msg.sender].debtAmount -= amount;
     _debtReserveBalance += amount;
@@ -121,6 +163,9 @@ contract Bank is BankStorage, Ownable, UsingTellor {
     emit VaultRepay(msg.sender, amount);
   }
 
+  /**
+  * @dev Allows users to withdraw their collateral from the vault
+  */
   function vaultWithdraw() external {
     require(vaults[msg.sender].debtAmount == 0, "DEBT OWED"); //should you not be able to just withdraw part?  
     require(IERC20(_collateralToken).transfer(msg.sender, vaults[msg.sender].collateralAmount));
--- contracts/BankStorage.sol
+++ contracts/BankStorage.sol
@@ -6,7 +6,7 @@ import '../node_modules/usingtellor/contracts/UsingTellor.sol';
 
 
 contract BankStorage{
-
+  /*Variables*/
   // TODO: Refactor into struct
   address _collateralToken;
   uint256 _collateralTokenPrice;
@@ -34,42 +34,73 @@ contract BankStorage{
     uint256 debtAmount;
     uint256 createdAt;
   }
-
+  
   mapping (address => Vault) public vaults;
 
   /////////////////////
   // SYSTEM PROPERTIES
   /////////////////////
 
+  /**
+  * @dev Getter function for the current interest rate
+  * @return interest rate
+  */
   function getInterestRate() public view returns (uint256) {
     return _interestRate;
   }
 
-
+  /**
+  * @dev Getter function for the origination fee
+  * @return origination fee
+  */
   function getOriginationFee() public view returns (uint256) {
     return _originationFee;
   }
 
+  /**
+  * @dev Getter function for the current collateralization ratio
+  * @return collateralization ratio
+  */
   function getCollateralizationRatio() public view returns (uint256) {
     return _collateralizationRatio;
   }
 
+  /**
+  * @dev Getter function for the liquidation penalty
+  * @return liquidation penalty
+  */
   function getLiquidationPenalty() public view returns (uint256) {
     return _liquidationPenalty;
   }
 
+  /**
+  * @dev Getter function for the debt token(reserve) price
+  * @return debt token price
+  */
   function getDebtTokenPrice() public view returns (uint256) {
     return _debtTokenPrice;
   }
 
+  /**
+  * @dev Getter function for the debt token price granularity
+  * @return debt token price granularity
+  */
   function getDebtTokenPriceGranularity() public view returns (uint256) {
     return _debtTokenPriceGranularity;
   }
 
+  /**
+  * @dev Getter function for the collateral token price 
+  * @return collateral token price
+  */
   function getCollateralTokenPrice() public view returns (uint256) {
     return _collateralTokenPrice;
   }
 
+  /**
+  * @dev Getter function for the collateral token price granularity
+  * @return collateral token price granularity
+  */
   function getCollateralTokenPriceGranularity() public view returns (uint256) {
     return _collateralTokenPriceGranularity;
   }
@@ -79,24 +110,42 @@ contract BankStorage{
   // RESERVE MANAGEMENT
   /////////////////////
 
+  /**
+  * @dev Getter function for the debt token(reserve) balance
+  * @return debt reserve balance
+  */
   function getReserveBalance() public view returns (uint256) {
     return _debtReserveBalance;
   }
 
+  /**
+  * @dev Getter function for the debt reserve collateral balance
+  * @return collateral reserve balance
+  */
   function getReserveCollateralBalance() public view returns (uint256) {
     return _collateralReserveBalance;
   }
 
-
+  /**
+  * @dev Getter function for the user's vault collateral amount
+  * @return collateral amount
+  */
   function getVaultCollateralAmount() public view returns (uint256) {
     return vaults[msg.sender].collateralAmount;
   }
 
+  /**
+  * @dev Getter function for the user's vault debt amount
+  * @return debt amount
+  */
   function getVaultDebtAmount() public view returns (uint256) {
     return vaults[msg.sender].debtAmount;
   }
 
-
+  /**
+  * @dev Getter function for the user's vault debt amount
+  * @return debt amount
+  */
   //I think there's a smarter way to do this than a loop...
   function getVaultRepayAmount() public view returns (uint256 principal) {
     principal = vaults[msg.sender].debtAmount;
@@ -104,6 +153,10 @@ contract BankStorage{
       principal += principal * _interestRate / 100 / 365;
   }
 
+  /**
+  * @dev Getter function for the collateralization ratio
+  * @return collateralization ratio
+  */
   function getVaultCollateralizationRatio(address vaultOwner) public view returns (uint256) {
     if(vaults[vaultOwner].debtAmount == 0 ){
       return 0;
@@ -114,6 +167,11 @@ contract BankStorage{
     }
   }
 
+  /**
+  * @dev This function calculates the percent of the given numerator, denominator to the
+  * specified precision
+  * @return debt amount
+  */
   function _percent(uint numerator, uint denominator, uint precision) private pure returns(uint256 _quotient) {
         _quotient =  ((numerator * 10 ** (precision+1) / denominator) + 5) / 10;
   }
