c3559339adf6e2e473dbe8e190e13b8032e80e1a
Michael Ghen <mike@mikeghen.com>
2020-09-11 12:27:13 -0400

    Saving dockerfile

---
+++ client/Dockerfile
@@ -0,0 +1,2 @@
+FROM nginx:1.19.0
+COPY build/ /usr/share/nginx/html
---
+++ client/build/precache-manifest.5c21395365e95200ae29276052b9b5f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5daf5e512f19b98f7d22f2b0a4c3cb4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dabf82150ba0b63cabe",
+    "url": "/static/css/2.d0affde7.chunk.css"
+  },
+  {
+    "revision": "493259d5aa139acf0f86",
+    "url": "/static/css/main.898527cf.chunk.css"
+  },
+  {
+    "revision": "7dabf82150ba0b63cabe",
+    "url": "/static/js/2.90955b32.chunk.js"
+  },
+  {
+    "revision": "1ec892735be7cf134fbdf53c6ab2c27f",
+    "url": "/static/js/2.90955b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493259d5aa139acf0f86",
+    "url": "/static/js/main.36275b29.chunk.js"
+  },
+  {
+    "revision": "22c5647dc9aeae946a2a",
+    "url": "/static/js/runtime-main.bc96b1cd.js"
+  }
+]);
